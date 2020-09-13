5c64fe6074bc471d672970cf0754c15934e03d27
jjs951213 <jjs951213@naver.com>
2020-09-14 01:55:14 +0900

    22.0.2 Request Body 검증

--- blog-backend/src/api/posts/post.ctrl.js
+++ blog-backend/src/api/posts/post.ctrl.js
@@ -1,5 +1,6 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
+import Joi from 'joi';
 
 
 const { ObjectId } = mongoose.Types;
@@ -14,60 +15,27 @@ export const checkObjectId = (ctx, next) => {
   }
   return next();
 };
-
-export const write = ctx => {};
-const { title, body, tags } = ctx.request.body;
-  const post = new Post({
-    title,
-    body,
-    tags,
-  });
-  try {
-    await post.save();
-    ctx.body = post;
-  } catch (e) {
-    ctx.throw(500, e);
-  }
-};
-
-
-
-export const list = async ctx => {
-  try {
-    const posts = await Post.find().exec();
-    ctx.body = posts;
-  } catch (e) {
-    ctx.throw(500, e);
-  }
-};
-
-export const read = async ctx => {
-  const { id } = ctx.params;
-  try {
-    const post = await Post.findById(id).exec();
-    if (!post) {
-      ctx.status = 404; // Not Found
-      return;
-    }
-    ctx.body = post;
-  } catch (e) {
-    ctx.throw(500, e);
-  }
-};
-
-export const remove = async ctx => {
-  const { id } = ctx.params;
-  try {
-    await Post.findByIdAndRemove(id).exec();
-    ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
-  } catch (e) {
-    ctx.throw(500, e);
-  }
-};
-
 export const update = async ctx => {
   const { id } = ctx.params;
-  try {
+  // write에서 사용한 schema와 비슷한데, required()가 없습니다.
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+
+// 검증하고 나서 검증 실패인 경우 에러 처리
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400; // Bad Request
+    ctx.body = result.error;
+    return;
+  }
+
+
+
+try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
       // false일 때는 업데이트되기 전의 데이터를 반환합니다.
