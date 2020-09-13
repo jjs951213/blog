1f55de7f753751bc2c5f3692b4de4572fd597fce
jjs951213 <jjs951213@naver.com>
2020-09-14 01:12:46 +0900

    22.4.1 기존 코드 ES module 형태로 바꾸기

--- blog-backend/src/api/index.js
+++ blog-backend/src/api/index.js
@@ -1,11 +1,9 @@
-const Router = require('koa-router');
-const api = new Router();
-
+import Router from 'koa-router';
+import posts from './posts';
 
 api.get('/test', ctx => {
   ctx.body = 'test 성공';
 });
 
 
-// 라우터를 내보냅니다.
-module.exports = api;
+export default api;
