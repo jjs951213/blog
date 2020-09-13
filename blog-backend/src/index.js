3ca3eb44221d0e939e4ec564ad94b639903cad25
jjs951213 <jjs951213@naver.com>
2020-09-14 00:51:49 +0900

    22.3.1 .env 환경변수 파일 생성

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT } = process.env;
 
 
 const api = require('./api');
@@ -20,7 +24,7 @@ app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 
 
-
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
 });
