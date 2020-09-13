b80e8329e0675ca1caa2cd80d8332da21665a833
jjs951213 <jjs951213@naver.com>
2020-09-14 01:33:43 +0900

    22.7.3 특정 포스트 조회

--- blog-backend/src/api/posts/post.ctrl.js
+++ blog-backend/src/api/posts/post.ctrl.js
@@ -26,9 +26,20 @@ export const list = async ctx => {
     ctx.throw(500, e);
   }
 };
-export const read = ctx => {};
 
-
+export const read = async ctx => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 export const remove = ctx => {};
 
