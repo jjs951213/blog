fbee11129a11643e55c3945ccabb1a0e6762efb1
jjs951213 <jjs951213@naver.com>
2020-09-14 02:39:07 +0900

    22.9.2 request body 검즌 2

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -85,10 +85,27 @@ export const remove = async ctx => {
     ctx.throw(500, e);
   }
 };
-
 export const update = async ctx => {
   const { id } = ctx.params;
-  try {
+  // write에서 사용한 schema와 비슷한데, required()가 없습니다.
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+
+// 검증하고 나서 검증 실패인 경우 에러 처리
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400; // Bad Request
+    ctx.body = result.error;
+    return;
+  }
+
+
+
+try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
       // false일 때는 업데이트되기 전의 데이터를 반환합니다.
