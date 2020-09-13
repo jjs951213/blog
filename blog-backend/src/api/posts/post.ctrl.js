a05b208203b9255a0c16e641ceeaf0defb61b4ac
jjs951213 <jjs951213@naver.com>
2020-09-14 01:50:21 +0900

    22.9.1 ObjectId 검증

--- blog-backend/src/api/posts/post.ctrl.js
+++ blog-backend/src/api/posts/post.ctrl.js
@@ -1,5 +1,19 @@
 import Post from '../../models/post';
+import mongoose from 'mongoose';
 
+
+const { ObjectId } = mongoose.Types;
+
+
+
+export const checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400; // Bad Request
+    return;
+  }
+  return next();
+};
 
 export const write = ctx => {};
 const { title, body, tags } = ctx.request.body;
