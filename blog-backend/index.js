aed3f38f97461328634eaa8ffb65cf75b943f71f
jjs951213 <jjs951213@naver.com>
2020-09-13 19:35:11 +0900

    21.3.2 미들웨어

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -4,6 +4,28 @@ const Koa = require('koa');
 const app = new Koa();
 
 
+app.use((ctx, next) => {
+    console.log(ctx.url);
+    console.log(1);
+    if (ctx.query.authorized !== '1') {
+        ctx.status = 401; // Unauthorized
+        return;
+      }
+      next();
+    });
+    
+    
+
+  
+  
+  
+  app.use((ctx, next) => {
+    console.log(2);
+    next();
+  });
+  
+  
+  
 
 app.use(ctx => {
   ctx.body = 'hello world';
