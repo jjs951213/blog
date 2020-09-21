ab7459cba834fe64b2102f56e009ddfc9ed6a3f8
jjs951213 <jjs951213@naver.com>
2020-09-22 02:30:52 +0900

    24.2.4 회원가입 구현

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
 
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
-    form: auth.register
+  const { form } = useSelector(({ auth,user }) => ({
+    form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError
+    user: user.user
   }));
   // 인풋 변경 이벤트 핸들러
   const onChange = e => {
@@ -21,12 +26,14 @@ const RegisterForm = () => {
     );
   };
 
-
-
-// 폼 등록 이벤트 핸들러
   const onSubmit = e => {
     e.preventDefault();
-    // 구현 예정
+    const { username, password, passwordConfirm } = form;
+    if (password !== passwordConfirm) {
+      // TODO: 오류 처리
+      return;
+    }
+    dispatch(register({ username, password }));
   };
 
 
@@ -35,6 +42,28 @@ const RegisterForm = () => {
   useEffect(() => {
     dispatch(initializeForm('register'));
   }, [dispatch]);
+
+
+
+// 회원가입 성공/실패 처리
+  useEffect(() => {
+    if (authError) {
+      console.log('오류 발생');
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log('회원가입 성공');
+      console.log(auth);
+    }
+  }, [auth, authError,dispatch]);
+
+
+  useEffect(() => {
+    if (user) {
+      history.push('/'); // 홈 화면으로 이동
+    } 
+  }, [history, user]);
 
 
 
@@ -51,3 +80,4 @@ return (
 
 
 export default RegisterForm;
+
