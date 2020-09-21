03ef42b4a0b059629f436b8eac9daf741bb0f6b0
jjs951213 <jjs951213@naver.com>
2020-09-22 01:01:52 +0900

    24.2.1.2-3 마무리

--- blog-frontend/src/components/auth/AuthForm.js
+++ blog-frontend/src/components/auth/AuthForm.js
@@ -46,29 +46,48 @@ const StyledInput = styled.input`
         `;
 
 
-const AuthForm = () => {
-    return (
-      <AuthFormBlock>
-        <h3>로그인</h3>
-        <form>
-          <StyledInput autoComplete="username" name="username" placeholder="아이디" />
-          <StyledInput 
-            autoComplete="new-password" 
-            name="password" 
-            placeholder="비밀번호"
-            type="password"
-          />
-          <ButtonWithMarginTop cyan fullWidth>
-              로그인
-            </ButtonWithMarginTop>
-        </form>
-        <Footer>
-          <Link to="/register">회원가입</Link>
-        </Footer>
-      </AuthFormBlock>
-    );
-  };
-  
-  
-  
-  export default AuthForm;
+        const textMap = {
+            login: '로그인',
+            register: '회원가입',
+          };
+          
+          
+          const AuthForm = ({ type }) => {
+            const text = textMap[type];
+            return (
+              <AuthFormBlock>
+                <h3>{text}</h3>
+                <form>
+                  <StyledInput autoComplete="username" name="username" placeholder="아이디" />
+                  <StyledInput
+                    autoComplete="new-password"
+                    name="password"
+                    placeholder="비밀번호"
+                    type="password"
+                  />
+                  {type === 'register' && (
+                    <StyledInput
+                      autoComplete="new-password"
+                      name="passwordConfirm"
+                      placeholder="비밀번호 확인"
+                      type="password"
+                    />
+                  )}
+                  <ButtonWithMarginTop cyan fullWidth style={{ marginTop: '1rem' }}>
+                    {text}
+                  </ButtonWithMarginTop>
+                </form>
+                <Footer>
+                  {type === 'login' ? (
+                    <Link to="/register">회원가입</Link>
+                  ) : (
+                    <Link to="/login">로그인</Link>
+                  )}
+                </Footer>
+              </AuthFormBlock>
+            );
+          };
+          
+          
+          
+          export default AuthForm;
