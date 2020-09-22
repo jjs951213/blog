d68153dca6baf182903b925788dbcc9ac29430b0
jjs951213 <jjs951213@naver.com>
2020-09-23 01:53:25 +0900

    26.1.2 API 연동하기

--- blog-frontend/src/modules/index.js
+++ blog-frontend/src/modules/index.js
@@ -4,12 +4,15 @@ import auth,{authSaga} from './auth';
 import loading from './loading';
 import user, { userSaga } from './user';
 import write, { writeSaga } from './write';
+import post, { postSaga } from './post';
+
 
 const rootReducer = combineReducers({
   auth,
   loading,
   user,
   write,
+  post,
 });
 
 export function* rootSaga() {
