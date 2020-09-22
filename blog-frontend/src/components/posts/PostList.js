d10a665ea1b138e3de124ae31ee1d1ee490ef6b1
jjs951213 <jjs951213@naver.com>
2020-09-23 03:11:07 +0900

    26.2.2 포스트 목록 조회 API 연동하기

--- blog-frontend/src/components/posts/PostList.js
+++ blog-frontend/src/components/posts/PostList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import Responsive from '../common/Responsive';
 import Button from '../common/Button';
 import palette from '../../lib/styles/palette';
@@ -79,36 +80,53 @@ const Tags = styled.div`
 `;
 
 
-const PostItem = () => {
+const PostItem = ({ post }) => {
+    const { publishedDate, user, tags, title, body, _id } = post;
     return (
         <PostItemBlock>
-          <h2>제목</h2>
-          <SubInfo username="username" publishedDate={new Date()} />
-          <Tags tags={['태그1', '태그2', '태그3']} />
-          <p>포스트 내용의 일부분..</p>
+          <h2>
+            <Link to ={`/@${user.username}/${_id}`}>{title}</Link>
+            </h2>
+          <SubInfo 
+            username={user.username}
+            publishedDate={new Date(publishedDate)}
+            />
+          <Tags tags={tags} />
+          <p>{body}</p>
         </PostItemBlock>
       );
     };
 
 
-
-const PostList = () => {
-  return (
-    <PostListBlock>
-      <WritePostButtonWrapper>
-        <Button cyan to="/write">
-          새 글 작성하기
-        </Button>
-      </WritePostButtonWrapper>
-      <div>
-        <PostItem />
-        <PostItem />
-        <PostItem />
-      </div>
-    </PostListBlock>
-  );
-};
-
-
-
-export default PostList;
+    const PostList = ({ posts, loading, error, showWriteButton }) => {
+        // 에러 발생 시
+        if (error) {
+          return <PostListBlock>에러가 발생했습니다.</PostListBlock>;
+        }
+      
+      
+      
+      return (
+          <PostListBlock>
+            <WritePostButtonWrapper>
+              {showWriteButton && (
+                <Button cyan to="/write">
+                  새 글 작성하기
+                </Button>
+              )}
+            </WritePostButtonWrapper>
+            {/*  로딩 중이 아니고, 포스트 배열이 존재할 때만 보여 줌 */}
+            {!loading && posts && (
+              <div>
+                {posts.map(post => (
+                  <PostItem post={post} key={post._id} />
+                ))}
+              </div>
+            )}
+          </PostListBlock>
+        );
+      };
+      
+      
+      
+      export default PostList;
