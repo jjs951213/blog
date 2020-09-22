d489e5f65c608207caffa7d5487f2a373514f710
jjs951213 <jjs951213@naver.com>
2020-09-22 23:06:19 +0900

    23.3.2.3 로그인 검증 실패시 정보 초기화

--- blog-frontend/src/modules/user.js
+++ blog-frontend/src/modules/user.js
@@ -20,11 +20,22 @@ export const check = createAction(CHECK);
 
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
-export function* userSaga() {
-  yield takeLatest(CHECK, checkSaga);
+
+
+function checkFailureSaga() {
+  try {
+    localStorage.removeItem('user'); // localStorage에서 user를 제거
+  } catch (e) {
+    console.log('localStorage is not working');
+  }
 }
 
 
+
+export function* userSaga() {
+  yield takeLatest(CHECK, checkSaga);
+  yield takeLatest(CHECK_FAILURE, checkFailureSaga);
+}
 
 const initialState = {
   user: null,
