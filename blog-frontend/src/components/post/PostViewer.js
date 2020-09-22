2ac43f2e85b35c4834a7d0360e606d0549a993cf
jjs951213 <jjs951213@naver.com>
2020-09-23 02:55:28 +0900

    26.2.1-2 PostList UI 준비하기-2

--- blog-frontend/src/components/post/PostViewer.js
+++ blog-frontend/src/components/post/PostViewer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
+import SubInfo from '../common/SubInfo';
+import Tags from '../common/Tags';
 
 
 const PostViewerBlock = styled(Responsive)`
@@ -70,17 +72,12 @@ const PostViewer = ({ post, error, loading }) => {
       <PostViewerBlock>
         <PostHead>
           <h1>{title}</h1>
-          <SubInfo>
-            <span>
-              <b>{user.username}</b>
-            </span>
-            <span>{new Date(publishedDate).toLocaleDateString()}</span>
-          </SubInfo>
-          <Tags>
-            {tags.map(tag => (
-              <div className="tag">#{tag}</div>
-            ))}
-          </Tags>
+          <SubInfo
+          username={user.username}
+          publishedDate={publishedDate}
+          hasMarginTop
+        />
+        <Tags tags={tags} />
         </PostHead>
         <PostContent dangerouslySetInnerHTML={{ __html: body }} />
       </PostViewerBlock>
--- blog-frontend/src/components/posts/PostList.js
+++ blog-frontend/src/components/posts/PostList.js
@@ -1,8 +1,10 @@
-import React from ‘react‘;
-import styled from ‘styled-components‘;
-import Responsive from ‘../common/Responsive‘;
-import Button from ‘../common/Button‘;
-import palette from ‘../../lib/styles/palette‘;
+import React from 'react';
+import styled from 'styled-components';
+import Responsive from '../common/Responsive';
+import Button from '../common/Button';
+import palette from '../../lib/styles/palette';
+import SubInfo from '../common/SubInfo';
+import Tags from '../common/Tags';
 
 
 const PostListBlock = styled(Responsive)`
@@ -78,23 +80,15 @@ const Tags = styled.div`
 
 
 const PostItem = () => {
-  return (
-    <PostItemBlock>
-      <h2>제목</h2>
-      <SubInfo>
-        <span>
-          <b>username</b>
-        </span>
-        <span>{new Date().toLocaleDateString()}</span>
-      </SubInfo>
-      <Tags>
-        <div className="tag">#태그1</div>
-        <div className="tag">#태그2</div>
-      </Tags>
-      <p>포스트 내용의 일부분..</p>
-    </PostItemBlock>
-  );
-};
+    return (
+        <PostItemBlock>
+          <h2>제목</h2>
+          <SubInfo username="username" publishedDate={new Date()} />
+          <Tags tags={['태그1', '태그2', '태그3']} />
+          <p>포스트 내용의 일부분..</p>
+        </PostItemBlock>
+      );
+    };
 
 
 
